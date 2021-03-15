497b5aebbf941d277ca119ecd053fc4da2d3464a
hj.seong <hj.seong@lgresearch.ai>
2021-03-15 15:43:33 +0900

    블렌딩 캔버스 크기 계산

--- js/main.js
+++ js/main.js
@@ -97,6 +97,13 @@
       objs: {
         container: document.querySelector("#scroll-section-3"),
         canvasCaption: document.querySelector(".canvas-caption"),
+        canvas: document.querySelector(".image-blend-canvas"),
+        context: document.querySelector(".image-blend-canvas").getContext("2d"),
+        imagesPath: [
+          "../images/blend-image-1.jpg",
+          "../images/blend-image-2.jpg",
+        ],
+        images: [],
       },
       values: {},
     },
@@ -116,6 +123,13 @@
       imgElem2 = new Image();
       imgElem2.src = `../video/002/IMG_${7027 + i}.JPG`;
       sceneInfo[2].objs.videoImages.push(imgElem2);
+    }
+
+    let imgElem3;
+    for (let i = 0; i < sceneInfo[3].objs.imagesPath.length; i++) {
+      imgElem3 = new Image();
+      imgElem3.src = sceneInfo[3].objs.imagesPath[i];
+      sceneInfo[3].objs.images.push(imgElem3);
     }
   }
   setCanvasImage();
@@ -291,7 +305,6 @@
         let sequence2 = Math.round(
           calcValues(values.imageSequence, currentYOffset)
         );
-        console.log(sequence2);
         objs.context.drawImage(objs.videoImages[sequence2], 0, 0);
 
         if (scrollRatio <= 0.5) {
@@ -388,6 +401,19 @@
         }
         break;
       case 3:
+        // 가로 세로 모두 꽉 차게 하기 위해 여기서 세팅 (계산 필요)
+        const widthRatio = window.innerWidth / objs.canvas.width;
+        const heightRatio = window.innerHeight / objs.canvas.height;
+        let canvasScaleRatio;
+
+        if (widthRatio <= heightRatio) {
+          canvasScaleRatio = heightRatio;
+        } else {
+          canvasScaleRatio = widthRatio;
+        }
+
+        objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
+        objs.context.drawImage(objs.images[0], 0, 0);
         break;
     }
   }
